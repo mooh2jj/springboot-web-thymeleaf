b0f17dbc3c4ed71183748c5ce87a56463ca55aa5
mooh2jj <mooh2jj@naver.com>
2021-11-12 22:41:00 +0900

    [add&update] validation setting

--- src/main/java/com/dsgcode/myhome/controller/BoardController.java
+++ src/main/java/com/dsgcode/myhome/controller/BoardController.java
@@ -2,13 +2,14 @@ package com.dsgcode.myhome.controller;
 
 import com.dsgcode.myhome.model.Board;
 import com.dsgcode.myhome.repository.BoardRepository;
+import com.dsgcode.myhome.validator.BoardValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/board")
@@ -16,6 +17,7 @@ import java.util.List;
 public class BoardController {
 
     private final BoardRepository boardRepository;
+    private final BoardValidator boardValidator;
 
     @GetMapping("/list")
     public String list(Model model) {
@@ -23,5 +25,27 @@ public class BoardController {
         var boards = boardRepository.findAll();
         model.addAttribute("boards", boards);
         return "board/list";
+    }
+
+
+    @GetMapping("/form")
+    public String boardForm(Model model, @RequestParam(required = false)Long id) {
+        if (id == null) {
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElseThrow(() -> new RuntimeException());
+            model.addAttribute("board", board);
+        }
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String boardSubmit(@ModelAttribute @Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:list";
     }
 }
